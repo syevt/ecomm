b192d059f9a1ef5e1c54b6694960fc8aa9151713
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-05-13 14:17:30 +0300

    Import Cart services

---
+++ app/services/ecomm/cart/calculate_cart_totals.rb
@@ -0,0 +1,16 @@
+module Ecomm
+  module Cart
+    class CalculateCartTotals < BaseService
+      def call(session)
+        cart = session[:cart]
+        cut = session[:discount]
+        products = Product.find cart.keys
+        items_total = cart.values.each_with_index.sum do |quantity, index|
+          quantity * products[index].price
+        end
+        discount = cut ? (items_total * cut / 100) : 0.0
+        [items_total, items_total - discount, discount]
+      end
+    end
+  end
+end
