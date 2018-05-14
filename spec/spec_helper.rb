d1007280550d868973109040ae13374a7a0ee35f
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-05-14 16:03:47 +0300

    Fix require rails-controller-testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require 'rails-controller-testing'
 require 'rspec/rails'
 require 'factory_girl_rails'
 
