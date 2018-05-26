f60f5b821990aaf704c315d788cbf1e28050dc73
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-05-26 15:43:13 +0300

    Fix Address and CreditCard factories

--- spec/factories/ecomm_credit_cards.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :ecomm_credit_card, class: 'Ecomm::CreditCard' do
-    number "MyString"
-    cardholder "MyString"
-    month_year "MyString"
-    cvv "MyString"
-    order_id 1
-  end
-end
