6956d52363847e4826258507d7215870cba63ead
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-07-07 13:49:39 +0300

    Add Ecomm setup method

--- lib/ecomm.rb
+++ lib/ecomm.rb
@@ -6,7 +6,7 @@ module Ecomm
   mattr_accessor :customer_class, :product_class,
                  :current_customer_method, :completed_order_url_helper_method,
                  :flash_login_return_to, :flash_not_authenticated_message_key,
-                 :signin_path, :catalog_path, :session_customer_id_key
+                 :signin_path, :catalog_path
 
   class << self
     def customer_class
@@ -15,6 +15,10 @@ module Ecomm
 
     def product_class
       @@product_class.constantize
+    end
+
+    def setup
+      yield self
     end
   end
 end
