c5e925411236d7196c10fd5076f2fefce0982a6d
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-08-22 15:54:35 +0300

    Add forgotten line_item factory

--- spec/factories/shareable/line_items.rb
+++ spec/factories/shareable/line_items.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     quantity 1
     ids = [*1..3]
     sequence(:product_id) { |n| ids[(n - 1) % ids.length] }
+
+    factory :line_item_with_product_id_cycled do
+      ids = [*1..4]
+      sequence(:product_id) { |n| ids[(n - 1) % ids.length] }
+    end
   end
 end
