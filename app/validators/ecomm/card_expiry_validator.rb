0d18803a51c68bd0278424a6ec807a4bcd08fc3d
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-08-23 12:01:22 +0300

    Fix card expiry date validator i18n translations

--- app/validators/ecomm/card_expiry_validator.rb
+++ app/validators/ecomm/card_expiry_validator.rb
@@ -4,11 +4,10 @@ module Ecomm
       month, year = record.month_year.split('/')
       expiry = DateTime.new(('20' << year).to_i, month.to_i)
       return if expiry >= DateTime.now.beginning_of_month
-      record.errors[:month_year] << I18n.t(
-        'errors.attributes.month_year.past_date'
-      )
+      tr_prefix = 'activemodel.errors.models.credit_card.attributes.month_year.'
+      record.errors[:month_year] << I18n.t("#{tr_prefix}past_date")
     rescue
-      record.errors[:month_year] << I18n.t('errors.attributes.month_year.invalid')
+      record.errors[:month_year] << I18n.t("#{tr_prefix}invalid")
     end
   end
 end
