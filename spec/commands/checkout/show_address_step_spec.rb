7cc9a5075064825d98c63a52e55bebda7ce05d68
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-09-08 11:50:30 +0300

    Remove @countries check from ShowAddressStep command spec

--- spec/commands/checkout/show_address_step_spec.rb
+++ spec/commands/checkout/show_address_step_spec.rb
@@ -22,37 +22,32 @@ describe Ecomm::Checkout::ShowAddressStep, type: :command do
       let(:get_totals) { double('CalculateCartTotals', call: [5.0, 7.0]) }
       let(:initialized_order) { build(:order, customer: customer) }
       let(:initializer) { double('InitializeOrder', call: initialized_order) }
-      let(:countries) { ['Mali'] }
-      let(:get_countries) { double('GetCountries', call: countries) }
 
       it 'assigns session order totals' do
         session_order = build(:order, customer: customer)
         builder = double('BuildOrder', call: session_order)
-        command = described_class.new(get_totals, builder, initializer,
-                                      get_countries)
+        command = described_class.new(get_totals, builder, initializer)
         command.call(session, {}, 1)
         expect(session[:items_total]).to eq(5)
         expect(session[:order_subtotal]).to eq(7)
       end
 
       it 'with order existing in session '\
-      'publishes :ok event passing session order and countries variables' do
+      'publishes :ok event passing session order' do
         session_order = build(:order, customer: customer)
         builder = double('BuildOrder', call: session_order)
-        command = described_class.new(get_totals, builder, initializer,
-                                      get_countries)
+        command = described_class.new(get_totals, builder, initializer)
         expect { command.call(session, {}, 1) }.to(
-          publish(:ok, order: session_order, countries: countries)
+          publish(:ok, order: session_order)
         )
       end
 
       it 'with no order in session publishes :ok event '\
-      'passing new initialized order and countries variables' do
+      'passing new initialized order' do
         builder = double('BuildOrder', call: nil)
-        command = described_class.new(get_totals, builder, initializer,
-                                      get_countries)
+        command = described_class.new(get_totals, builder, initializer)
         expect { command.call(session, {}, 1) }.to(
-          publish(:ok, order: initialized_order, countries: countries)
+          publish(:ok, order: initialized_order)
         )
       end
     end
