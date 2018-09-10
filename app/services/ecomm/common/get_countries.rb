3979616c2dec04a43736d2eead56bc8feb06bc3d
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-09-10 18:32:44 +0300

    Change countries retrieval in GetCountries service

--- app/services/ecomm/common/get_countries.rb
+++ app/services/ecomm/common/get_countries.rb
@@ -2,7 +2,7 @@ module Ecomm
   module Common
     class GetCountries < BaseService
       def call
-        Country.all.sort.map { |country| [country.name, country.country_code] }
+        Country.all { |_x, data| [data['name'], data['country_code']] }.sort
       end
     end
   end
